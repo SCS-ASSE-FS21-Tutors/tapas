5d37aea3d7862f0fb2411ee2e56b98f14b92f4af
Marcel <marcel@maece.net>
2021-10-06 18:11:18 +0200

    added code quality scanns

--- assignment/src/main/java/ch/unisg/assignment/AssignmentApplication.java
+++ assignment/src/main/java/ch/unisg/assignment/AssignmentApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AssignmentApplication {
 
 	public static void main(String[] args) {
+		System.out.println("TEST SONAR");
 		SpringApplication.run(AssignmentApplication.class, args);
 	}
 
