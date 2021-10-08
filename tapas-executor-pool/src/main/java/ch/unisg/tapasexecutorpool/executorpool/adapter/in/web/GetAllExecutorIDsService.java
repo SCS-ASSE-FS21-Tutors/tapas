03d9e42fb8630627669e1f7d31722eb7eb59ad98
AlexanderLontke <lontke.alexander@yahoo.com>
2021-10-08 11:52:51 +0200

    Added TODOs

---
+++ tapas-executor-pool/src/main/java/ch/unisg/tapasexecutorpool/executorpool/adapter/in/web/GetAllExecutorIDsService.java
@@ -0,0 +1,16 @@
+package ch.unisg.tapasexecutorpool.executorpool.adapter.in.web;
+
+import ch.unisg.tapasexecutorpool.executorpool.application.port.in.GetAllExecutorIDsUseCase;
+import ch.unisg.tapasexecutorpool.executorpool.domain.Executor;
+import ch.unisg.tapasexecutorpool.executorpool.domain.ExecutorList;
+
+import java.util.List;
+
+public class GetAllExecutorIDsService implements GetAllExecutorIDsUseCase {
+    @Override
+    public List<Integer> getAllExecutorIDs() {
+        // TODO your implementation here
+
+        return null;
+    }
+}
--- tapas-executor-pool/src/main/java/ch/unisg/tapasexecutorpool/executorpool/domain/ExecutorList.java
+++ tapas-executor-pool/src/main/java/ch/unisg/tapasexecutorpool/executorpool/domain/ExecutorList.java
@@ -1,4 +1,5 @@
 package ch.unisg.tapasexecutorpool.executorpool.domain;
 
 public class ExecutorList {
+    // TODO: Implement as singleton (https://www.journaldev.com/1377/java-singleton-design-pattern-best-practices-examples)
 }
