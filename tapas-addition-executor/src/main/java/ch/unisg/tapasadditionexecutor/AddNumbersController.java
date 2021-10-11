4e2b9040cf8de9aa05fbb7f5ad4ad9769d948ba8
AlexanderLontke <lontke.alexander@yahoo.com>
2021-10-11 11:33:57 +0200

    Simplified executor and removed parameters

--- tapas-addition-executor/src/main/java/ch/unisg/tapasadditionexecutor/AddNumbersController.java
+++ tapas-addition-executor/src/main/java/ch/unisg/tapasadditionexecutor/AddNumbersController.java
@@ -1,28 +1,34 @@
 package ch.unisg.tapasadditionexecutor;
 
+import org.json.JSONObject;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 
 
 @RestController
 public class AddNumbersController {
-    private final AddNumbersUseCase addNumbersUseCase;
 
-    public AddNumbersController(AddNumbersUseCase addNumbersUseCase) {
-        this.addNumbersUseCase = addNumbersUseCase;
-    }
+    @GetMapping(path = "/addition")
+    public ResponseEntity<String> addNumbers() {
+        Random rand = new Random(123);
+        float operator1 = rand.nextFloat();
+        float operator2 = rand.nextFloat();
 
-    @PostMapping(path = "/addition", consumes = "application/json")
-    public ResponseEntity<Float> addNumbers(@RequestBody AddNumbersCommand addNumbersCommand) {
-        Float result = addNumbersUseCase.addNumbers(addNumbersCommand);
+        JSONObject payload = new JSONObject();
+
+        payload.put("operator1", operator1);
+        payload.put("operator2", operator2);
+        payload.put("result", operator1+operator2);
+
         // Add the content type as a response header
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.add(HttpHeaders.CONTENT_TYPE, "application/json");
-        return new ResponseEntity<Float>(result, responseHeaders, HttpStatus.CREATED);
-
+        return new ResponseEntity<>(payload.toString(), responseHeaders, HttpStatus.CREATED);
     }
 }
--- tapas-addition-executor/src/main/java/ch/unisg/tapasadditionexecutor/AddNumbersUseCase.java
+++
@@ -1,5 +0,0 @@
-package ch.unisg.tapasadditionexecutor;
-
-public interface AddNumbersUseCase {
-    float addNumbers(AddNumbersCommand command);
-}
