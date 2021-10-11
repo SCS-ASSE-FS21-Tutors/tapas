e7bd3e28e83c54324c89108ebee1fe2aa8a8bc57
yasmineantille <66117613+yasmineantille@users.noreply.github.com>
2021-10-11 11:20:16 +0200

    created executor application

---
+++ tapas-executors/src/test/java/ch/unisg/tapasexecutors/TapasExecutorsApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.unisg.tapasexecutors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapasExecutorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
