6e9164a7a0eb6e821e4479ff600de03dd5e86e53
jo9x <jonas.hermann@web.de>
2021-10-11 16:07:33 +0200

    Implemented Task and Task Queue

--- tapas-roster/src/main/java/ch/unisg/tapasroster/roster/domain/Task.java
+++ tapas-roster/src/main/java/ch/unisg/tapasroster/roster/domain/Task.java
@@ -1,4 +1,34 @@
 package ch.unisg.tapasroster.roster.domain;
 
+import lombok.Getter;
+import lombok.Value;
+
+import java.util.UUID;
+
+/**This is a domain entity**/
 public class Task {
+
+    @Getter
+    private final TaskId taskId;
+
+    @Getter
+    private final TaskType taskType;
+
+
+    public Task(TaskId taskId, TaskType taskType) {
+        this.taskType = taskType;
+        this.taskId = taskId;
+    }
+
+
+    @Value
+    public static class TaskId {
+        private String value;
+    }
+
+
+    @Value
+    public static class TaskType {
+        private String value;
+    }
 }
--- tapas-roster/src/main/java/ch/unisg/tapasroster/roster/domain/TaskQueue.java
+++ tapas-roster/src/main/java/ch/unisg/tapasroster/roster/domain/TaskQueue.java
@@ -1,4 +1,31 @@
 package ch.unisg.tapasroster.roster.domain;
 
+import lombok.Getter;
+import lombok.Value;
+
+import java.util.LinkedList;
+
 public class TaskQueue {
+
+    @Getter
+    private final QueueOfTasks queueOfTasks;
+
+    //Singleton Implementation
+    private static final TaskQueue taskQueue = new TaskQueue();
+    private TaskQueue() {
+        this.queueOfTasks = new QueueOfTasks(new LinkedList<Task>());
+    }
+
+    public void addTaskToQueue(Task task){
+        this.queueOfTasks.value.add(task);
+    }
+
+    public Task getNextTask(){
+        return this.queueOfTasks.value.getFirst();
+    }
+
+    @Value
+    public static class QueueOfTasks {
+        private LinkedList<Task> value;
+    }
 }
