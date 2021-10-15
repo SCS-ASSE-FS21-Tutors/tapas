3cd46f5677b940756c7555b3d577152c38ab37a7
yasmineantille <66117613+yasmineantille@users.noreply.github.com>
2021-10-15 16:33:42 +0200

    * updated RobotExecutor with functionality

--- tapas-executors/src/main/java/ch/unisg/tapasexecutors/executors/domain/RobotExecutor.java
+++ tapas-executors/src/main/java/ch/unisg/tapasexecutors/executors/domain/RobotExecutor.java
@@ -1,83 +1,179 @@
 package ch.unisg.tapasexecutors.executors.domain;
 
+import ch.unisg.tapastasks.tasks.domain.Task;
 import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
+@Component
 public class RobotExecutor implements Executors {
 
     @Getter
-    private final ExecutorName executorName;
+    private final ExecutorName executorName = new ExecutorName("CherrybotExecutor");
 
     @Getter
-    private final ExecutorType executorType;
+    private final Task.TaskType taskType = new Task.TaskType("RobotTask");
 
     @Getter
-    private final ExecutorState executorState;
+    @Setter
+    private ExecutorState executorState;
 
     @Getter
     private ExecutorId executorId;
 
+    private static final HttpClient httpClient = HttpClient.newBuilder()
+            .version(HttpClient.Version.HTTP_2)
+            .connectTimeout(Duration.ofSeconds(10))
+            .build();
 
-    public RobotExecutor(ExecutorName executorName, ExecutorType executorType) {
-        this.executorName = executorName;
-        this.executorType = executorType;
+    public RobotExecutor() {
         this.executorState = new ExecutorState(State.IDLE);
         this.executorId = new ExecutorId(UUID.randomUUID().toString());
     }
 
-    protected static RobotExecutor createExecutorWithNameAndType(ExecutorName name, ExecutorType type) {
-        // for debugging
-        System.out.println("New Executor: " + name.getValue() + " " + type.getValue());
-        return new RobotExecutor(name,type);
+    /**
+     * Sends POST /operator request to gain access to the robot
+     */
+    public static String registerOperator() throws IOException, InterruptedException {
+        String json = "{\"name\":\"Tapas Team 2\",\"email\":\"team2@unisg.ch\"}";
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(json))
+                .uri(URI.create("https://api.interactions.ics.unisg.ch/cherrybot/operator"))
+                .setHeader("Content-Type", "application/json")
+                //.header("X-API-KEY", "opensesame")
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        // print status code
+        System.out.println(response.statusCode());
+
+        Optional<String> delete_url = response.headers().firstValue("Location");
+        return delete_url.get();
     }
 
-    public static void startExecutor() {
-        try {
-            URL url = new URL("https://api.interactions.ics.unisg.ch/cherrybot/operator");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("POST");
-            conn.setDoOutput(true);     // is this necessary?
+    /**
+     * Sends PUT /tcp/target to set the robot's point target to which it will move
+     */
+    public static void makeRobotDoStuff(String token) throws IOException, InterruptedException {
+        // json formatted data
+        String json = new StringBuilder()
+                .append("{")
+                .append("\"target\": {")
+                .append("\"coordinate\": {")
+                .append("\"x\": 600,")
+                .append("\"y\": 0,")
+                .append("\"z\": 600")
+                .append("},")
+                .append("\"rotation\": {")
+                .append("\"roll\": 0,")
+                .append(" \"pitch\": 0,")
+                .append("\"yaw\": 180")
+                .append("}")
+                .append("},")
+                .append("\"speed\": 50")
+                .append("}").toString();
 
-            byte[] out = "{\"name\":\"Jan Tapas\",\"email\":\"janliam.albert@unisg.ch\"}" .getBytes(StandardCharsets.UTF_8);
-            int length = out.length;
+        HttpRequest request = HttpRequest.newBuilder()
+                .PUT(HttpRequest.BodyPublishers.ofString(json))
+                .uri(URI.create("https://api.interactions.ics.unisg.ch/cherrybot/tcp/target"))
+                .setHeader("Content-Type", "application/json")
+                .header("Authentication", token)
+                .build();
 
-            conn.setFixedLengthStreamingMode(length);
-            conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-            conn.connect();
-            try(OutputStream os = conn.getOutputStream()) {
-                os.write(out);
-            }
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
-            StringBuilder res = new StringBuilder();
-            try (BufferedReader buf = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
-                for (String line; (line = buf.readLine()) != null;) {
-                    res.append(line);
-                }
-            }
-        } catch (IOException e) {
-            System.out.println("Probably another operator is currently using the robot");
-            e.printStackTrace();
-        }
+        // print status code
+        System.out.println(response.statusCode());
+    }
 
-        System.out.println("Connected to robot");
+    /**
+     * Sends PUT /initialize to reset the robot
+     */
+    public static void initRob(String token) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .PUT(HttpRequest.BodyPublishers.noBody())
+                .uri(URI.create("https://api.interactions.ics.unisg.ch/cherrybot/initialize"))
+                .setHeader("Authentication", token)
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        // print status code
+        System.out.println(response.statusCode());
+    }
+
+    public static void getRobotStatus(String token) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create("https://api.interactions.ics.unisg.ch/cherrybot/tcp"))
+                .setHeader("Authentication", token)
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        // print status code
+        System.out.println(response.statusCode());
+        System.out.println(response.body());
+    }
+
+    /**
+     * Sends DELETE /operator request to delete the current operator
+     * @param deleteurl
+     */
+    public static void delete(String deleteurl) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .DELETE()
+                .uri(URI.create(deleteurl))
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        // print status code
+        System.out.println(response.statusCode());
     }
 
     @Override
     public void startTask() {
-        // TODO
+        executorState = new ExecutorState(State.BUSY);
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            // ignore
+        }
+        execute();
     }
 
+    private void sleeper() {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            // ignore
+        }
+    }
     @Override
     public void execute() {
-        // TODO
+//        String deleteurl = registerOperator();
+//        sleeper();
+//        String token = deleteurl.substring(deleteurl.lastIndexOf("/") + 1);
+//        makeRobotDoStuff(token);
+//        sleeper();
+//        delete(deleteurl);
     }
 
     @Override
@@ -85,31 +181,61 @@ public class RobotExecutor implements Executors {
         // TODO
     }
 
+    private static void getCurrent() throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create("https://api.interactions.ics.unisg.ch/cherrybot/operator"))
+                //.setHeader("Content-Type", "application/json")
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        // print status code
+        System.out.println(response.statusCode());
+        System.out.println(response.body());
+    }
+
 
     /* simple getter connection to robot as example */
-    private static void connectRobot() throws IOException {
+    private static void getCurrentOperator() throws IOException {
         URL url = new URL("https://api.interactions.ics.unisg.ch/cherrybot/operator");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
+
         StringBuilder res = new StringBuilder();
         try (BufferedReader buf = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
             for (String line; (line = buf.readLine()) != null;) {
                 res.append(line);
             }
         }
-        if (res.equals("204 No Content")) {
-            System.out.println("There is nobody operating the robot at this point in time.");
-        } else {
-            System.out.println("Current operator: " + res);
-        }
+        System.out.println(res);
     }
 
     /**
      * Main method for quick terminal debugging purposes
      */
-    public static void main(String[] args) throws IOException {
-        connectRobot();
-        // startTask();
-        // startExecutor();
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // getCurrent();
+        String deleteurl = registerOperator();
+        System.out.println(deleteurl);
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            // ignore
+        }
+        String token = deleteurl.substring(deleteurl.lastIndexOf("/") + 1);
+        getRobotStatus(token);
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            // ignore
+        }
+        makeRobotDoStuff(token);
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            // ignore
+        }
+        delete(deleteurl);
     }
 }
