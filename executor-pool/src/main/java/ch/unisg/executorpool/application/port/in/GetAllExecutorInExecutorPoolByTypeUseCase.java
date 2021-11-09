8cfdd5ff094fd8e31c569a46e862928e8aea7d8a
reynisson <reynissonkristofer@gmail.com>
2021-11-09 21:25:02 +0100

    Fixed some spelling mistakes and fixed an error in the Task list that produced a build error

--- executor-pool/src/main/java/ch/unisg/executorpool/application/port/in/GetAllExecutorInExecutorPoolByTypeUseCase.java
+++
@@ -1,9 +0,0 @@
-package ch.unisg.executorpool.application.port.in;
-
-import ch.unisg.executorpool.domain.ExecutorClass;
-
-import java.util.List;
-
-public interface GetAllExecutorInExecutorPoolByTypeUseCase {
-    List<ExecutorClass> getAllExecutorInExecutorPoolByType(GetAllExecutorInExecutorPoolByTypeQuery query);
-}
