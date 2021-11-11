d54b2313d1d3d4ebe91d00ebcb5a8e8985dd99a4
Gian-Luca Frei <gianlucafrei@gmail.com>
2021-11-11 13:46:49 +0100

    Updated task domain objects

--- tapas-roster/src/main/java/ch/unisg/tapas/roster/entities/Task.java
+++ tapas-roster/src/main/java/ch/unisg/tapas/roster/entities/Task.java
@@ -4,57 +4,80 @@ import lombok.*;
 
 import java.util.UUID;
 
-@Data
+/**This is a domain entity**/
+@NoArgsConstructor
+@AllArgsConstructor
 public class Task {
 
-    public enum State {
+    public enum Status {
         OPEN, ASSIGNED, RUNNING, EXECUTED
     }
 
     @Getter
-    private final TaskId taskId;
+    private TaskId taskId;
 
     @Getter
-    private final TaskName taskName;
+    private TaskName taskName;
 
     @Getter
-    private final TaskType taskType;
+    private TaskType taskType;
 
     @Getter
-    private TaskState taskState;
+    private OriginalTaskUri originalTaskUri;
+
+    @Getter @Setter
+    private TaskStatus taskStatus;
+
+    @Getter @Setter
+    private ServiceProvider provider;
+
+    @Getter @Setter
+    private InputData inputData;
+
+    @Getter @Setter
+    private OutputData outputData;
 
     public Task(TaskName taskName, TaskType taskType) {
+        this.taskId = new TaskId(UUID.randomUUID().toString());
         this.taskName = taskName;
         this.taskType = taskType;
-        this.taskState = new TaskState(State.OPEN);
+        this.originalTaskUri = null;
+        this.taskStatus = new TaskStatus(Status.OPEN);
+    }
+
+    public Task(TaskName taskName, TaskType taskType, OriginalTaskUri taskUri) {
         this.taskId = new TaskId(UUID.randomUUID().toString());
+        this.taskName = taskName;
+        this.taskType = taskType;
+        this.originalTaskUri = taskUri;
+        this.taskStatus = new TaskStatus(Status.OPEN);
+        this.inputData = null;
+        this.outputData = null;
     }
 
     protected static Task createTaskWithNameAndType(TaskName name, TaskType type) {
         //This is a simple debug message to see that the request has reached the right method in the core
         System.out.println("New Task: " + name.getValue() + " " + type.getValue());
-        return new Task(name,type);
+        return new Task(name, type, null);
+    }
+
+    protected static Task createTaskWithNameAndTypeAndOriginalTaskUri(TaskName name, TaskType type,
+                                                                      OriginalTaskUri originalTaskUri) {
+        return new Task(name, type, originalTaskUri);
     }
 
     @Value
     @NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
     @AllArgsConstructor
     public static class TaskId {
-        private String value;
+        String value;
     }
 
     @Value
     @NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
     @AllArgsConstructor
     public static class TaskName {
-        private String value;
-    }
-
-    @Value
-    @NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
-    @AllArgsConstructor
-    public static class TaskState {
-        private State value;
+        String value;
     }
 
     @Value
@@ -62,5 +85,40 @@ public class Task {
     @AllArgsConstructor
     public static class TaskType {
         private String value;
+    }
+
+    @Value
+    @NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
+    @AllArgsConstructor
+    public static class OriginalTaskUri {
+        String value;
+    }
+
+    @Value
+    @NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
+    @AllArgsConstructor
+    public static class TaskStatus {
+        Status value;
+    }
+
+    @Value
+    @NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
+    @AllArgsConstructor
+    public static class ServiceProvider {
+        String value;
+    }
+
+    @Value
+    @NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
+    @AllArgsConstructor
+    public static class InputData {
+        String value;
+    }
+
+    @Value
+    @NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
+    @AllArgsConstructor
+    public static class OutputData {
+        String value;
     }
 }
