9293697eff654cd714d457c2acf7c96bef8d791c
Gian-Luca Frei <gianlucafrei@gmail.com>
2021-11-14 13:52:15 +0100

    Moved bootstrap methods into own components

--- tapas-auction-house/src/main/java/ch/unisg/tapas/TapasAuctionHouseApplication.java
+++ tapas-auction-house/src/main/java/ch/unisg/tapas/TapasAuctionHouseApplication.java
@@ -21,54 +21,9 @@ import java.util.List;
 @OpenAPIDefinition(info = @Info(title = "Auction-House-API", version = "1"))
 @SpringBootApplication
 public class TapasAuctionHouseApplication {
-    private static final Logger LOGGER = LogManager.getLogger(TapasAuctionHouseApplication.class);
-
-    public static String RESOURCE_DIRECTORY = "https://api.interactions.ics.unisg.ch/auction-houses/";
-    public static String MQTT_BROKER = "tcp://broker.hivemq.com:1883";
 
     public static void main(String[] args) {
 		SpringApplication tapasAuctioneerApp = new SpringApplication(TapasAuctionHouseApplication.class);
-
-		// We will use these bootstrap methods in Week 6:
-        bootstrapMarketplaceWithWebSub();
-        bootstrapMarketplaceWithMqtt();
-
         tapasAuctioneerApp.run(args);
 	}
-
-    /**
-     * Discovers auction houses and subscribes to WebSub notifications
-     */
-	private static void bootstrapMarketplaceWithWebSub() {
-        List<String> auctionHouseEndpoints = discoverAuctionHouseEndpoints();
-        LOGGER.info("Found auction house endpoints: " + auctionHouseEndpoints);
-
-        WebSubSubscriber subscriber = new WebSubSubscriber();
-
-        for (String endpoint : auctionHouseEndpoints) {
-            subscriber.subscribeToAuctionHouseEndpoint(URI.create(endpoint));
-        }
-    }
-
-    /**
-     * Connects to an MQTT broker, presumably the one used by all TAPAS groups to communicate with
-     * one another
-     */
-    private static void bootstrapMarketplaceWithMqtt() {
-        try {
-            AuctionEventsMqttDispatcher dispatcher = new AuctionEventsMqttDispatcher();
-            TapasMqttClient client = TapasMqttClient.getInstance(MQTT_BROKER, dispatcher);
-            client.startReceivingMessages();
-        } catch (MqttException e) {
-            LOGGER.error(e.getMessage(), e);
-        }
-    }
-
-	private static List<String> discoverAuctionHouseEndpoints() {
-        AuctionHouseResourceDirectory rd = new AuctionHouseResourceDirectory(
-            URI.create(RESOURCE_DIRECTORY)
-        );
-
-	    return rd.retrieveAuctionHouseEndpoints();
-    }
 }
---
+++ tapas-auction-house/src/main/java/ch/unisg/tapas/auctionhouse/adapter/in/messaging/websub/WebSubMarketplaceBootstrapper.java
@@ -0,0 +1,45 @@
+package ch.unisg.tapas.auctionhouse.adapter.in.messaging.websub;
+
+import ch.unisg.tapas.auctionhouse.adapter.common.clients.WebSubSubscriber;
+import ch.unisg.tapas.common.AuctionHouseResourceDirectory;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.net.URI;
+import java.util.List;
+
+@Log4j2
+@Component
+public class WebSubMarketplaceBootstrapper {
+
+   @Value("${ch.unisg.tapas.resource-directory}")
+    private String resourceDirectory;
+
+    /**
+     * Discovers auction houses and subscribes to WebSub notifications
+     */
+    @EventListener(ContextRefreshedEvent.class)
+    private void bootstrapMarketplaceWithWebSub() {
+        List<String> auctionHouseEndpoints = discoverAuctionHouseEndpoints();
+        log.info("Found auction house endpoints: " + auctionHouseEndpoints);
+
+        WebSubSubscriber subscriber = new WebSubSubscriber();
+
+        for (String endpoint : auctionHouseEndpoints) {
+            subscriber.subscribeToAuctionHouseEndpoint(URI.create(endpoint));
+        }
+
+        log.info("Started WebSub Adapter");
+    }
+
+    private List<String> discoverAuctionHouseEndpoints() {
+        AuctionHouseResourceDirectory rd = new AuctionHouseResourceDirectory(
+            URI.create(resourceDirectory)
+        );
+
+        return rd.retrieveAuctionHouseEndpoints();
+    }
+}
