86fea4254ce138b5fe32449e1bbd69e0d059a38c
Ruben Gonzalez <rubengaviles@gmail.com>
2021-11-14 22:14:55 +0100

    cleaned up place bid method

--- tapas-auction-house/src/main/java/ch/unisg/tapas/auctionhouse/adapter/out/web/PlaceBidForAuctionCommandHttpAdapter.java
+++ tapas-auction-house/src/main/java/ch/unisg/tapas/auctionhouse/adapter/out/web/PlaceBidForAuctionCommandHttpAdapter.java
@@ -5,10 +5,6 @@ import ch.unisg.tapas.auctionhouse.application.port.out.PlaceBidForAuctionComman
 import ch.unisg.tapas.auctionhouse.application.port.out.PlaceBidForAuctionCommandPort;
 import ch.unisg.tapas.auctionhouse.domain.Auction;
 import ch.unisg.tapas.auctionhouse.domain.Bid;
-import ch.unisg.tapas.auctionhouse.domain.Task;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
@@ -17,18 +13,13 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-/**
- * This class is a tempalte for implementing a place bid for auction command via HTTP.
- */
 @Component
 @Primary
 public class PlaceBidForAuctionCommandHttpAdapter implements PlaceBidForAuctionCommandPort {
 
     private HttpClient client;
-    private ObjectMapper om;
 
     public PlaceBidForAuctionCommandHttpAdapter() {
-        this.om = new ObjectMapper();
         this.client = HttpClient.newHttpClient();
     }
 
@@ -42,7 +33,7 @@ public class PlaceBidForAuctionCommandHttpAdapter implements PlaceBidForAuctionC
             String bidJson = BidJsonRepresentation.serialize(bid);
 
             HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(auction.getAuctionHouseUri().getValue() + "/bid/"))
+                .uri(URI.create(auction.getAuctionHouseUri().getValue() + "bid/"))
                 .headers("Content-Type", BidJsonRepresentation.MEDIA_TYPE)
                 .POST(HttpRequest.BodyPublishers.ofString(bidJson))
                 .build();
