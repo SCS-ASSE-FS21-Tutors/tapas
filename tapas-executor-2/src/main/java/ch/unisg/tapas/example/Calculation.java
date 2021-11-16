3ea26f094f93adfb6411ccd2ff2bd94d6cf34b31
Ruben Gonzalez <rubengaviles@gmail.com>
2021-11-16 20:05:15 +0100

    hardcoded correct executorpool uri

--- tapas-executor-2/src/main/java/ch/unisg/tapas/example/Calculation.java
+++ tapas-executor-2/src/main/java/ch/unisg/tapas/example/Calculation.java
@@ -20,7 +20,7 @@ import java.util.concurrent.TimeUnit;
 @RequiredArgsConstructor
 public class Calculation {
 
-    private String executorPoolUri = "http://localhost:8083/";
+    private String executorPoolUri = "http://tapas-executor-pool:8083";
 
     @NonNull
     private String taskId;
