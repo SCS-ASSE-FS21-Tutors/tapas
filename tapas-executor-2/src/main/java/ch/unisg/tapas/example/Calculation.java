e5bef2c4388af62b2e65fa9c61c77db8a1f882e8
Ruben Gonzalez <rubengaviles@gmail.com>
2021-11-22 10:58:31 +0100

    Using Jannis calculation logic

--- tapas-executor-2/src/main/java/ch/unisg/tapas/example/Calculation.java
+++ tapas-executor-2/src/main/java/ch/unisg/tapas/example/Calculation.java
@@ -12,6 +12,8 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 
@@ -34,10 +36,53 @@ public class Calculation {
 
         try {
 
-            ScriptEngineManager mgr = new ScriptEngineManager();
-            ScriptEngine engine = mgr.getEngineByName("js");
+            List<String> operators = new ArrayList<>();
+            List<String> operands = new ArrayList<>();
 
-            result = (int) engine.eval(inputData);
+            String temp = null;
+
+            for(int i=0; i<inputData.length(); i++){
+                if(inputData.charAt(i) == '+'){
+                    operands.add("+");
+                    operators.add(temp);
+                    temp = "";
+                }else if(inputData.charAt(i) == '-'){
+                    operands.add("-");
+                    operators.add(temp);
+                    temp = "";
+                }else if(inputData.charAt(i) == '*'){
+                    operands.add("*");
+                    operators.add(temp);
+                    temp = "";
+                }else if(inputData.charAt(i) == '/'){
+                    operands.add("/");
+                    operators.add(temp);
+                    temp = "";
+                }else {
+                    if(i==0){
+                        temp = String.valueOf(inputData.charAt(i));
+                    }else if(i==inputData.length()-1){
+                        temp = temp + String.valueOf(inputData.charAt(i));
+                        operators.add(temp);
+                    }
+                    else {
+                        temp = temp + String.valueOf(inputData.charAt(i));
+                    }
+                }
+            }
+
+            int output = Integer.parseInt(operators.get(0));
+            for(int i=0; i<operands.size(); i++){
+                if(operands.get(i) == "+"){
+                    output += Integer.parseInt(operators.get(i+1));
+                }else if(operands.get(i) == "-"){
+                    output -= Integer.parseInt(operators.get(i+1));
+                }else if(operands.get(i) == "*"){
+                    output *= Integer.parseInt(operators.get(i+1));
+                }
+            }
+            result = output;
+
             TimeUnit.SECONDS.sleep(10);
             // Calls the /completion/ endpoint of the executor pool
             HttpClient client = HttpClient.newHttpClient();
