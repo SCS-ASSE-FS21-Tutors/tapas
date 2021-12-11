a2982727189fffe33e8567e78ac0f051b6c23a94
Ruben Gonzalez <rubengaviles@gmail.com>
2021-12-11 11:16:24 +0100

    Retrieving miro card URI from search engine

--- tapas-executor-3/src/main/java/ch/unisg/tapas/example/application/CoAPRequestThread.java
+++ tapas-executor-3/src/main/java/ch/unisg/tapas/example/application/CoAPRequestThread.java
@@ -11,19 +11,21 @@ import ch.unisg.tapas.example.formats.TaskJsonRepresentation;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.log4j.Log4j2;
-import org.json.JSONObject;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.ByteArrayInputStream;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
 @Log4j2
 public class CoAPRequestThread extends Thread {
-
-    // Temporary hardcoded address with thing description of miro card
-    private final String miroCardTdUri = "http://yggdrasil.interactions.ics.unisg.ch/environments/61/workspaces/102/artifacts/mirogate";
 
     private HttpClient client;
     private ObjectMapper objectMapper;
@@ -43,55 +45,62 @@ public class CoAPRequestThread extends Thread {
     @Override
     public void run() {
 
-        // Get the form for the relevant operation from the SparQL Search engine and TD
-        Optional<Form> formOptional = getFormFromSearchEngine();
+        Optional<String> miroCardUri = getMiroCardUriFromSearchEngine();
+        if (miroCardUri.isPresent()) {
+            // Get the form for the relevant operation from the SparQL Search engine and TD
+            Optional<Form> formOptional = getFormFromMiroCardUri(miroCardUri.get());
+            // Execute CoAP request
+            if (formOptional.isPresent()) {
+                Form form = formOptional.get();
+                log.info("Sending request to CoAP Server: " + form.getTarget());
+                TDCoapRequest request = new TDCoapRequest(form, TD.readProperty);
+                try {
+                    // Wait for response from the CoAP Server and parse actual result from response
+                    TDCoapResponse response = request.execute();
+                    if (response.getPayload().isEmpty()) {
+                        throw new RuntimeException("No payload received from CoAP request");
+                    }
+                    String result = objectMapper.readValue(response.getPayload().get(), JsonNode.class).get("value").toString();
+                    log.info("CoAP Response received: " + result);
 
-        // Execute CoAP request
-        if (formOptional.isPresent()) {
-            Form form = formOptional.get();
-            log.info("Sending request to CoAP Server: " + form.getTarget());
-            TDCoapRequest request = new TDCoapRequest(form, TD.readProperty);
-            try {
-                // Wait for response from the CoAP Server and parse actual result from response
-                TDCoapResponse response = request.execute();
-                if (response.getPayload().isEmpty()) {
-                    throw new RuntimeException("No payload received from CoAP request");
+                    // Send result back to executor pool
+                    String url = executorPoolUri + "completion";
+                    log.info("Sending result to Executor pool at: " + url);
+                    task.setOutputData(result);
+                    String bodyString = objectMapper.writeValueAsString(task);
+                    HttpRequest executorPoolRequest = HttpRequest.newBuilder()
+                            .uri(URI.create(url))
+                            .headers("Content-Type", TaskJsonRepresentation.MEDIA_TYPE)
+                            .PUT(HttpRequest.BodyPublishers.ofString(bodyString))
+                            .build();
+                    HttpResponse executorPoolResponse = client.send(executorPoolRequest, HttpResponse.BodyHandlers.ofString());
+                    if (executorPoolResponse.statusCode() != 200) {
+                        throw new RuntimeException("Executor pool responded with unexpected status code " +
+                                executorPoolResponse.statusCode() + " instead of 200");
+                    } else {
+                        log.info("Result received by executor pool");
+                    }
+
+                } catch (Exception e) {
+                    log.error("Processing CoAP Request failed");
+                    e.printStackTrace();
                 }
-                String result = objectMapper.readValue(response.getPayload().get(), JsonNode.class).get("value").toString();
-                log.info("CoAP Response received: " + result);
 
-                // Send result back to executor pool
-                String url = executorPoolUri + "completion";
-                log.info("Sending result to Executor pool at: " + url);
-                task.setOutputData(result);
-                String bodyString = objectMapper.writeValueAsString(task);
-                HttpRequest executorPoolRequest = HttpRequest.newBuilder()
-                        .uri(URI.create(url))
-                        .headers("Content-Type", TaskJsonRepresentation.MEDIA_TYPE)
-                        .PUT(HttpRequest.BodyPublishers.ofString(bodyString))
-                        .build();
-                HttpResponse executorPoolResponse = client.send(executorPoolRequest, HttpResponse.BodyHandlers.ofString());
-                if (executorPoolResponse.statusCode() != 200) {
-                    throw new RuntimeException("Executor pool responded with unexpected status code " +
-                            executorPoolResponse.statusCode() + " instead of 200");
-                }
-
-            } catch (Exception e) {
-                log.error("Processing CoAP Request failed");
-                e.printStackTrace();
+            } else {
+                throw new RuntimeException("No humidity property could be found on the Miro Card TD");
             }
 
-        } else {
-            throw new RuntimeException("Input data was not identified as possible property in TD");
-        }
+        } else throw new RuntimeException("No Miro card URI could be retrieved from Search Engine");
 
     }
 
-    private Optional<Form> getFormFromSearchEngine() {
+    private Optional<String> getMiroCardUriFromSearchEngine() {
         log.info("Sending search query to search engine: " + sparqlSearchEngineUri);
 
-        /* TODO Implement querying of search engine to retrieve Miro Card TD
-        String sparqlQuery ="@prefix td: <https://www.w3.org/2019/wot/td#>.\n" +
+        // Querying of search engine to retrieve Miro Card TD URI
+        Optional<String> miroCardUri = Optional.empty();
+
+        String sparqlQuery = "@prefix td: <https://www.w3.org/2019/wot/td#>.\n" +
                 "select ?x\n" +
                 "where { ?x a td:Thing }";
 
@@ -100,19 +109,41 @@ public class CoAPRequestThread extends Thread {
                 .headers("Content-Type", "application/sparql-query")
                 .POST(HttpRequest.BodyPublishers.ofString(sparqlQuery))
                 .build();
+
         try {
             HttpResponse sparqlResponse = client.send(sparqlRequest, HttpResponse.BodyHandlers.ofString());
-            log.info(sparqlResponse.body());
-        } catch (Exception e ){
+            // Read values from XML-response and search for mirogate URI
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            StringBuilder xmlStringBuilder = new StringBuilder();
+
+            String xmlString = sparqlResponse.body().toString().replace("\n", "");
+            xmlStringBuilder.append(xmlString);
+            ByteArrayInputStream input = new ByteArrayInputStream(
+                    xmlStringBuilder.toString().getBytes(StandardCharsets.UTF_8));
+            Document doc = builder.parse(input);
+            doc.getDocumentElement().normalize();
+            NodeList resultElements = doc.getElementsByTagName("result");
+            for (int i = 0; i < resultElements.getLength(); i++) {
+                String retrievedUri = resultElements.item(i).getFirstChild().getFirstChild().getTextContent();
+                if (retrievedUri.contains("mirogate")) {
+                    miroCardUri = Optional.of(retrievedUri);
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            log.error("Querying search engine failed");
             e.printStackTrace();
         }
-        */
+        return miroCardUri;
+    }
 
-        // Retrieve the endpoints for all defined operations and store them in HashMap
-        log.info("Retrieving readProperty-form from things description of miro card: " + miroCardTdUri);
+    public Optional<Form> getFormFromMiroCardUri(String miroCardUri) {
+        // Retrieve the possible properties of the TD and return the one for humidity
+        log.info("Retrieving readProperty-form from things description of miro card: " + miroCardUri);
         Optional<Form> resultForm = Optional.empty();
         try {
-            ThingDescription td = TDGraphReader.readFromURL(ThingDescription.TDFormat.RDF_TURTLE, miroCardTdUri);
+            ThingDescription td = TDGraphReader.readFromURL(ThingDescription.TDFormat.RDF_TURTLE, miroCardUri);
             Optional<PropertyAffordance> property = td.getPropertyByName("humidity");
             if (property.isPresent()) {
                 Optional<Form> form = property.get().getFirstFormForOperationType(TD.readProperty);
